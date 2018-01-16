0cab022ab96120281758cb6daf0cfff86ea18237
anwar03 <anwarazim03@gmail.com>
2018-01-16 14:10:28 +0600

    Created account settings update view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
 from django.contrib.auth import login as auth_login
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
+
 
 from .forms import signUpForm
 # Create your views here.
@@ -17,3 +23,14 @@ def signup(request):
         form = signUpForm()
     
     return render(request, 'signup.html',  { 'form': form })
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'accounts.html'
+    fields = ('username','first_name','last_name', 'email')
+    success_url = reverse_lazy('my_accounts')
+
+    def get_object(self):
+        return self.request.user
+
